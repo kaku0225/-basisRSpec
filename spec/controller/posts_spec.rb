4cac12bf633ef620611d08382e9d2368a7e47bc9
eason <acontentedmind@gmail.com>
2021-01-27 18:01:43 +0800

    create有不一樣

--- spec/controller/posts_spec.rb
+++ spec/controller/posts_spec.rb
@@ -1,15 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  before(:all) do
+    @post_1 = Post.create(title: 'title_1', content: 'content_1')
+    @post_2 = Post.create(title: 'title_2', content: 'content_2')
+  end
+
   it '#index' do
     get :index
     expect(response).to have_http_status(200)
     expect(response).to render_template(:index)
   end
 
-  it 'new' do 
+  it '#new' do 
     get :new
     expect(response).to have_http_status(200)
     expect(response).to render_template(:new)
+  end
+
+  it '#edit' do
+    get :edit, params: { id: @post_1.id }
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:edit)
+  end
+
+  describe '#create' do
+    before(:all) do
+      @post_params = {post: {title: 'title', content: 'content'}}
+    end
+    
+    it 'create record' do
+      expect{ post :create, params: @post_params }.to change{Post.all.size}.by(1)
+    end
   end
 end
